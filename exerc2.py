412a9d0188f9cb51d2944252a76239df6245717d
Matheus Borin <matheusborin349@gmail.com>
2026-05-04 20:45:10 -0300

    Upload do arquivo original .py

---
+++ exerc2.py
@@ -0,0 +1,10 @@
+#2. Faixa Etária
+
+idade = int(input("Digite sua idade: "))
+
+if idade < 12:
+    print("Criança")
+elif idade >= 12 and idade < 18:
+    print("Adolescente")
+elif idade >= 18 and idade < 60:
+    print("Adulto")
